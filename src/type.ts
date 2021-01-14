560c9136f49ec03f2bc9a11066d5f34a8ed99b53
Kothing <39289309+kothing@users.noreply.github.com>
2021-01-14 10:45:20 +0800

    Create type.ts

---
+++ src/type.ts
@@ -0,0 +1,16 @@
+export interface Dimensions {
+  readonly x: number;
+  readonly y: number;
+  readonly left: number;
+  readonly top: number;
+  readonly right: number;
+  readonly bottom: number;
+  readonly width: number;
+  readonly height: number;
+}
+
+export type UseDimensionsHook = [
+  (node: HTMLElement) => void,
+  {} | Dimensions,
+  HTMLElement
+];
